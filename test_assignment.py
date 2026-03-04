0c092096eb9ad5ce1d5c55bd05b5fa622ab464c6
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2026-03-04 10:36:58 +0800

    Update test_assignment.py

--- test_assignment.py
+++ test_assignment.py
@@ -1,10 +1,14 @@
-import pytest,inspect
+import pytest
+import inspect
 from assignment import *
+
 
 def check_contains_loop(func):
     source = inspect.getsource(func)
     return "for " in source or "while " in source
-    
+
+
+# Exercise 1
 @pytest.mark.parametrize("h,m,s,expected", [
     (1, 2, 3, 3723),
     (0, 0, 0, 0),
@@ -16,6 +20,8 @@ def check_contains_loop(func):
 def test1(h, m, s, expected):
     assert time_to_seconds(h, m, s) == expected
 
+
+# Exercise 2
 @pytest.mark.parametrize("y,m,expected", [
     (2, 7, 31),
     (0, 5, 5),
@@ -26,6 +32,8 @@ def test1(h, m, s, expected):
 def test2(y, m, expected):
     assert years_months_to_months(y, m) == expected
 
+
+# Exercise 3
 @pytest.mark.parametrize("a,b,expected", [
     (16, 14, 16),
     (5, 10, 10),
@@ -36,6 +44,8 @@ def test2(y, m, expected):
 def test3(a, b, expected):
     assert max_of_two(a, b) == expected
 
+
+# Exercise 4
 @pytest.mark.parametrize("a,b,c,d,expected", [
     (85, 75, 96, 69, 181),
     (81, 82, 83, 84, 330),
@@ -46,6 +56,8 @@ def test3(a, b, expected):
 def test4(a, b, c, d, expected):
     assert sum_greater_than_80(a, b, c, d) == expected
 
+
+# Exercise 5 (PRINT)
 @pytest.mark.parametrize("n,expected_output", [
     (1, "IOI\n"),
     (3, "IOI\nIOI\nIOI\n"),
@@ -55,34 +67,42 @@ def test5(capsys, n, expected_output):
     print_ioi(n)
     captured = capsys.readouterr()
     assert captured.out == expected_output
+    assert check_contains_loop(print_ioi)
 
-@pytest.mark.parametrize("n,expected_output", [
-    (0, "1\n"),
-    (1, "1\n"),
-    (5, "120\n"),
-    (7, "5040\n"),
+
+# Exercise 6 (RETURN)
+@pytest.mark.parametrize("n,expected", [
+    (0, 1),
+    (1, 1),
+    (5, 120),
+    (7, 5040),
 ])
-def test6(capsys, n, expected_output):
-    factorial(n)
+def test6(n, expected):
+    assert factorial(n) == expected
+
+
+# Exercise 7 (PRINT)
+def test7(capsys):
+    expected_output = (
+        "3*1=3\n"
+        "3*2=6\n"
+        "3*3=9\n"
+        "3*4=12\n"
+        "3*5=15\n"
+        "3*6=18\n"
+        "3*7=21\n"
+        "3*8=24\n"
+        "3*9=27\n"
+        "3*10=30\n"
+    )
+
+    multiplication_table(3)
     captured = capsys.readouterr()
     assert captured.out == expected_output
-
-def test7():
-    expected = [
-        "3*1=3",
-        "3*2=6",
-        "3*3=9",
-        "3*4=12",
-        "3*5=15",
-        "3*6=18",
-        "3*7=21",
-        "3*8=24",
-        "3*9=27",
-        "3*10=30",
-    ]
-    assert multiplication_table(3) == expected
+    assert check_contains_loop(multiplication_table)
 
 
+# Exercise 8 (PRINT)
 def test8(capsys):
     expected_output = (
         "1 \n"
@@ -91,7 +111,8 @@ def test8(capsys):
         "4 4 4 4 \n"
         "5 5 5 5 5 \n"
     )
-    print_pattern_1(5)
+
+    print_pattern(5)
     captured = capsys.readouterr()
     assert captured.out == expected_output
-    assert check_contains_loop(print_pattern_1)
+    assert check_contains_loop(print_pattern)
