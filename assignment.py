4a185d96a2533e24a668f983a87b279f28bc5f01
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2026-03-04 10:46:36 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,50 +1,31 @@
 # Exercise 1
-def time_to_seconds(hours, minutes, s):
-    return hours * 3600 + minutes * 60 + s
+def time_to_seconds(hours, minutes, secondst):
+    pass
 
 # Exercise 2
 def years_months_to_months(years, months):
-    return years * 12 + months
-
+    pass
 
 # Exercise 3
 def max_of_two(a, b):
-    if a > b:
-        return a
-    return b
+    pass
 
 # Exercise 4
 def sum_greater_than_80(a, b, c, d):
-    total = 0
-    for num in [a, b, c, d]:
-        if num > 80:
-            total += num
-    return total
+    pass
 
-
-# Exercise 5 (PRINT)
+# Exercise 5
 def print_ioi(n):
-    for _ in range(n):
-        print("IOI")
+    pass
 
-
-# Exercise 6 (RETURN)
+# Exercise 6
 def factorial(n):
-    result = 1
-    for i in range(1, n + 1):
-        result *= i
-    return result
+    pass
 
-
-# Exercise 7 (PRINT)
+# Exercise 7
 def multiplication_table(n):
-    for i in range(1, 11):
-        print(f"{n}*{i}={n*i}")
+    pass
 
-
-# Exercise 8 (PRINT)
+# Exercise 8
 def print_pattern(n):
-    for i in range(1, n + 1):
-        for _ in range(i):
-            print(i, end=" ")
-        print()
+    pass
